25916f38bddbbb409389c0504952eb9da77cdfad
Jiseungmin <wltmdals0118@sunmoon.ac.kr>
2024-10-31 01:33:04 +0900

    bluer

--- app/screens/decoScreen.js
+++ app/screens/decoScreen.js
@@ -1,12 +1,62 @@
-import React from "react";
-import { View, Text, StyleSheet, SafeAreaView, Image } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, SafeAreaView, Image, TouchableOpacity, FlatList } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { CustomBtn } from '../../src/components';
 
-
 const DecoScreen = () => {
     const route = useRoute();
-    const { selectedImage } = route.params; // 이전 화면에서 넘어온 이미지 URI를 가져옴
+    const { selectedImage, result } = route.params;
+
+    // 초기 상태로 selectedImage 설정
+    const [selectedItems, setSelectedItems] = useState([]);
+    const [displayedImage, setDisplayedImage] = useState(selectedImage);
+
+    // result 문자열을 JSON 객체로 변환
+    const parsedResult = JSON.parse(result);
+
+    // 텍스트와 boundingPolygon 정보 추출
+    const items = parsedResult.combined_result.map(item => ({
+        text: item.text,
+        boundingPolygon: item.boundingPolygon
+    }));
+
+    console.log("items: ", items)
+
+    // 항목을 선택하거나 선택 해제하는 함수
+    const toggleSelectItem = (item) => {
+        const index = selectedItems.findIndex(i => i.text === item.text);
+        if (index > -1) {
+            setSelectedItems(selectedItems.filter(i => i.text !== item.text));
+        } else {
+            setSelectedItems([...selectedItems, item]);
+        }
+    };
+
+    // 서버로 선택된 항목을 전송하는 함수
+    const sendSelectedItemsToServer = async () => {
+        console.log("IN")
+        try {
+            const response = await fetch("http://100.66.163.31:8000/bluer", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    items: selectedItems
+                })
+            });
+            if (response.ok) {
+                console.log("success1")
+                const data = await response.json();
+                if (data.blurred_image) {
+                    setDisplayedImage(`data:image/png;base64,${data.blurred_image}`);
+                }
+                console.log("Success2")
+            } else {
+                console.log("Failed to send data");
+            }
+        } catch (error) {
+            console.error("Error sending data to server:", error);
+        }
+    };
 
     return (
         <SafeAreaView style={styles.safeContainer}>
@@ -16,24 +66,40 @@ const DecoScreen = () => {
                     보호하고 싶은 부분을 멋지게 꾸며드릴게요.✨
                 </Text>
             </View>
-        {/* 선택된 이미지 표시 */}
-        {selectedImage ? (
-            <Image source={{ uri: selectedImage }} style={styles.image} />
-        ) : (
-            <Text style={styles.errorText}>이미지를 로드할 수 없습니다.</Text>
-        )}
-      
-        {/* 버튼 */}
-            <CustomBtn
-                title=" 개인정보 가리기 "
-                textStyle={{ fontFamily: "Jua", fontSize: 24 }}
-                buttonStyle={{ backgroundColor: '#4CAF50', width:200 }}
+
+            {/* 선택된 이미지 표시 */}
+            {displayedImage ? (
+                <Image source={{ uri: displayedImage }} style={styles.image} />
+            ) : (
+                <Text style={styles.errorText}>이미지를 로드할 수 없습니다.</Text>
+            )}
+
+            {/* 텍스트 목록 표시 */}
+            <FlatList
+                data={items}
+                keyExtractor={(item, index) => index.toString()}
+                contentContainerStyle={styles.listContainer}
+                renderItem={({ item }) => (
+                    <TouchableOpacity 
+                        onPress={() => toggleSelectItem(item)} 
+                        style={[styles.textItem, selectedItems.find(i => i.text === item.text) && styles.selectedTextItem]}
+                    >
+                        <View style={[styles.checkbox, selectedItems.find(i => i.text === item.text) && styles.checkedCheckbox]} />
+                        <Text style={styles.text}>{item.text}</Text>
+                    </TouchableOpacity>
+                )}
             />
-            <CustomBtn
-                title=" 저장 "
-                textStyle={{ fontFamily: "Jua", fontSize: 24 }}
-                buttonStyle={{ backgroundColor: '#4CAF50', width:200 }}
-            />
+                <CustomBtn
+                    title=" 개인정보 가리기 "
+                    onPress={sendSelectedItemsToServer}
+                    textStyle={{ fontFamily: "Jua", fontSize: 24 }}
+                    buttonStyle={{ backgroundColor: '#4CAF50', width: 200 }}
+                />
+                <CustomBtn
+                    title=" 저장 "
+                    textStyle={{ fontFamily: "Jua", fontSize: 24 }}
+                    buttonStyle={{ backgroundColor: '#4CAF50', width: 200 }}
+                />
         </SafeAreaView>
     );
 };
@@ -42,59 +108,82 @@ const styles = StyleSheet.create({
     safeContainer: {
         flex: 1,
         backgroundColor: "#ffffff",
-        justifyContent: "center",
+        justifyContent: "space-between",
         alignItems: "center",
         paddingHorizontal: 20,
+        paddingVertical: 20,
     },
     image: {
-        width: "80%",
-        height: "50%",
+        width: "90%",
+        height: 250,
         resizeMode: "contain",
         marginBottom: 20,
-    },
-    buttonContainer: {
-        flexDirection: "row",
-        justifyContent: "center",
-        width: "100%",
     },
     titleContainer: {
         width: '100%',
         paddingHorizontal: 25,
         marginBottom: 10,
     },
-  button: {
-    backgroundColor: "#4CAF50",
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 10,
-    marginHorizontal: 10,
-  },
-  buttonText: {
-    color: "#ffffff",
-    fontSize: 18,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  errorText: {
-    fontSize: 18,
-    color: "#ff0000",
-    textAlign: "center",
-    marginBottom: 20,
-  },
-  title: {
-    fontFamily: "Jua",
-    fontSize: 32,
-    fontWeight: 'bold',
-    textAlign: 'left',
-  },
-  subtitle: {
-    fontSize: 18,
-    marginBottom: 20,
-    fontWeight: "500",
-    color: "#555",
-    fontFamily: "Jua",
-    textAlign: 'left',
-  },
+    listContainer: {
+        paddingVertical: 10,
+        width: '100%'
+    },
+    textItem: {
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 12,
+        borderWidth: 1,
+        borderColor: "#ddd",
+        marginVertical: 5,
+        borderRadius: 5,
+        backgroundColor: "#f9f9f9",
+        width: '90%',
+        alignSelf: 'center',
+    },
+    selectedTextItem: {
+        backgroundColor: "#e0f7fa",
+        borderColor: "#00897b",
+    },
+    checkbox: {
+        width: 20,
+        height: 20,
+        marginRight: 10,
+        borderWidth: 1,
+        borderColor: "#ddd",
+        borderRadius: 5,
+    },
+    checkedCheckbox: {
+        backgroundColor: "#4CAF50",
+        borderColor: "#4CAF50",
+    },
+    text: {
+        fontSize: 16,
+    },
+    errorText: {
+        fontSize: 18,
+        color: "#ff0000",
+        textAlign: "center",
+        marginBottom: 20,
+    },
+    title: {
+        fontFamily: "Jua",
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'left',
+    },
+    subtitle: {
+        fontSize: 18,
+        marginBottom: 20,
+        fontWeight: "500",
+        color: "#555",
+        fontFamily: "Jua",
+        textAlign: 'left',
+    },
+    buttonContainer: {
+        marginTop: 20,
+        alignItems: "center",
+        width: '100%',
+    },
 });
 
 export default DecoScreen;
--- app/screens/mainScreen.js
+++ app/screens/mainScreen.js
@@ -5,11 +5,14 @@ import { CustomBtn } from '../../src/components';
 import { useRouter } from 'expo-router';
 
 const MainScreen = () => {
-    const [step, setStep] = useState(1);
-    const [selectedImage, setSelectedImage] = useState(null);
     const router = useRouter();
+    const [step, setStep] = useState(1);
+    const [result, setResult] = useState()
+    const [content, setContent] = useState()
+    const [imageBase64, setImageBase64] = useState(null);
+    const [selectedImage, setSelectedImage] = useState(null);
 
-    //갤러리 접근 & 사진 선택
+    // 갤러리 접근 & 사진 선택
     const pickImage = async () => {
         const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
         if (permissionResult.granted === false) {
@@ -22,18 +25,55 @@ const MainScreen = () => {
             allowsEditing: true,
             aspect: [3, 4],
             quality: 1,
+            base64: true
         });
 
         if (!result.canceled) {
             setSelectedImage(result.assets[0].uri);
+            setImageBase64(result.assets[0].base64);  
         }
     };
 
-    //이미지 전달
+     // 서버로 이미지 전송 (Base64 인코딩된 이미지)
+    const uploadImage = async () => {
+      if (!imageBase64) return;
+
+      try {
+          const response = await fetch("http://100.66.163.31:8000/upload", {
+              method: 'POST',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({ image: imageBase64 }),
+          });
+
+          if (response.ok) {
+              const data = await response.json();  
+              setResult(data.combined_result)
+              setContent(data.combined_result.content)
+              console.log("Image uploaded successfully");
+          } else {
+              console.log("Image upload failed", response.status);
+          }
+      } catch (error) {
+          console.error("Error uploading image:", error);
+      }
+  };
+  
+  
+    // "다음" 버튼 클릭 시 이미지 전송 및 다음 화면 이동
+    const nextStep = async() => {
+      if (step === 1 && selectedImage) {
+          setStep(2);
+          await uploadImage()
+      } else if (step === 2) {
+          setStep(3);
+      }
+  };
+
+    // 이미지 전달
     const goToDecoScreen = () => {
         router.push({
           pathname: 'screens/decoScreen',
-          params: { selectedImage }
+          params: { selectedImage, result: JSON.stringify(result) }
         });
     };
 
@@ -52,7 +92,7 @@ const MainScreen = () => {
           </View>
         )}
         {step === 3 && (
-          <Text style={styles.feedback}>사진 분석 결과를 여기에 표시합니다.</Text>
+          <Text style={styles.feedback}>{content}</Text>
         )}
       </>
     );
@@ -98,7 +138,7 @@ const MainScreen = () => {
             title="다음"
             textStyle={{ fontFamily: "Jua", fontSize: 24 }}
             buttonStyle={{ backgroundColor: '#4CAF50', width:100 }}
-            onPress={() => setStep(step + 1)}
+            onPress={nextStep}
           />
         )}
         {step === 3 && (
@@ -195,15 +235,15 @@ const styles = StyleSheet.create({
     height: '40%',
     resizeMode: 'contain',
     marginVertical: 10,
-    flex: 1,
-    marginBottom: 150,
+    flex: 1
   },
   feedback: {
     fontSize: 18,
-    marginBottom: 20,
+    marginBottom: 100,
     fontWeight: "500",
     color: "#555",
     fontFamily: "Jua",
+    padding:20
   },
 });
 
